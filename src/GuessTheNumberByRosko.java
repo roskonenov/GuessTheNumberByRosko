becc85020a4b1b2988a6034e8136cac8e64e80a4
Rosen <roskonenov@gmail.com>
2023-08-08 18:01:53 +0300

    Changed the name of the class

--- src/GuessTheNumberByRosko.java
+++ src/GuessTheNumberByRosko.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.Scanner;
 
 
-public class GuessTheNumber {
+public class GuessTheNumberByRosko {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
